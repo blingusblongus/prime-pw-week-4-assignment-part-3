5ef16098c7c459b3a09a37a5ba0050fac989fafa
Nick Allen <rutherford.t.b@gmail.com>
2021-09-08 00:32:23 -0500

    added and tested isFull

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,6 +3,7 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket = [];
+const maxItems = 5;
 
 function addItem(item){
   basket.push(item);
@@ -27,3 +28,15 @@ function empty(){
 
 empty();
 console.log('Test empty(), should be empty array:', basket);
+
+function isFull(){
+  if(basket.length < maxItems) return false;
+  return true;
+}
+
+console.log('Test isFull - should be false');
+basket = ['a','a','a','b'];
+console.log(isFull());
+console.log('Test isFull - should be true');
+basket = ['a','a','a','b','c'];
+console.log(isFull());
